64bf4afb313e0d6dc0a8e036e380eca93c056a94
Rich E <rich.eakin@gmail.com>
2020-12-20 19:49:31 -0500

    working intersection shader

--- ray_tracing__before/shaders/raycommon.glsl
+++ ray_tracing__before/shaders/raycommon.glsl
@@ -1,3 +1,18 @@
+struct Sphere
+{
+	vec3  center;
+	float radius;
+};
+
+struct Aabb
+{
+	vec3 minimum;
+	vec3 maximum;
+};
+
+#define KIND_SPHERE 0
+#define KIND_CUBE 1
+
 struct hitPayload
 {
   vec3 hitValue;
